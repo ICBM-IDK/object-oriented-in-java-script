79bab0b1e58c6fb265c8981b206dac5820c2ff97
ZCoates <coatezzz00@gmail.com>
2026-07-16 10:12:10 +0100

    New_recruit

--- DOGs_inheritance.js
+++ DOGs_inheritance.js
@@ -16,6 +16,11 @@ Dog.prototype.run = function (){
 Dog.prototype.wag = function (){
     console.log("wag")
 }
+
+Dog.prototype.sit = function (){
+    console.log(this.name + " is now sitting");
+}
+
 function Dog(name, breed, weight) {
     this.name =name;
     this.breed=breed;
@@ -25,8 +30,15 @@ function Dog(name, breed, weight) {
 var Private_fluffers = new Dog("Fluffers","poodle",30)
 var Sergeant_spot = new Dog("Spot","chihuahua",10)
 var Commander_fido = new Dog("Fido","mixed",38)
+var Brigadier_Barnaby = new Dog("Barnaby", "Basset Hound",55)
 var army = [Private_fluffers,Sergeant_spot,Commander_fido]
+
+Sergeant_spot.bark = function (){
+    alert(this.name + " says exterminate")
+};
 
 Private_fluffers.bark()
 Sergeant_spot.run()
 Commander_fido.wag()
+Sergeant_spot.bark()
+Brigadier_Barnaby.sit()
